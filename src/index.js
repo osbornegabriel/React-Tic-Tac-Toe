82c80abab3219ac493958f2001976d03af544735
Gabriel Osborne <osbornegabriel@gmail.com>
2019-03-12 16:54:23 -0400

    Show past moves

--- src/index.js
+++ src/index.js
@@ -139,6 +139,17 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    })
+
     let status;
     if (winner) {
       status = 'Winner ' + winner;
@@ -156,8 +167,8 @@ class Game extends React.Component {
           />
         </div>
         <div className="game-info">
-          <div>{ status }</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
